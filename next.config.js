c14f77398db5d11b4407ca51a4c4db49515e66ec
Mateus Neri <mateuslimaneri@gmail.com>
2023-12-28 10:05:58 -0300

    #13 - Removed next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    webpack: function (config, options) {
-        console.log(options.webpack.version); // Should be webpack v5 now
-        config.experiments = {};
-        return config;
-    },
-};
